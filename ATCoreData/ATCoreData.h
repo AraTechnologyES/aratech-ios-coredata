0f45e0587d49f0cb9c89ca97207aabc92c647582
Nicolas Landa <nicolas.landa@ara-tech.es>
2018-07-04 09:40:16 +0200

    Initial Commit

---
+++ ATCoreData/ATCoreData.h
@@ -0,0 +1,19 @@
+//
+//  ATCoreData.h
+//  ATCoreData
+//
+//  Created by Nicolás Landa on 4/7/18.
+//  Copyright © 2018 Aratech. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ATCoreData.
+FOUNDATION_EXPORT double ATCoreDataVersionNumber;
+
+//! Project version string for ATCoreData.
+FOUNDATION_EXPORT const unsigned char ATCoreDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ATCoreData/PublicHeader.h>
+
+
