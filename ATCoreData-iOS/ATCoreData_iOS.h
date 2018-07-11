872400a00cf7fb42ad0b8e4a1e1457294276159e
machukas <machukkas@gmail.com>
2018-07-11 12:34:32 +0200

    New: Añadido Target iOS

---
+++ ATCoreData-iOS/ATCoreData_iOS.h
@@ -0,0 +1,19 @@
+//
+//  ATCoreData_iOS.h
+//  ATCoreData-iOS
+//
+//  Created by Nicolás Landa on 11/7/18.
+//  Copyright © 2018 Aratech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ATCoreData_iOS.
+FOUNDATION_EXPORT double ATCoreData_iOSVersionNumber;
+
+//! Project version string for ATCoreData_iOS.
+FOUNDATION_EXPORT const unsigned char ATCoreData_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ATCoreData_iOS/PublicHeader.h>
+
+
